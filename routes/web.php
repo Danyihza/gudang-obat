a1b13104157a5c1aefd5684e873ce873d7bcd8fb
Danyihza <danyahmadihza99@gmail.com>
2021-12-21 14:47:48 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('auth.loginView');
 });
 
 Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
